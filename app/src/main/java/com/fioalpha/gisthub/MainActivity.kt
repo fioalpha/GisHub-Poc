6a8c06f60ac2cdf9f48bb22a56f4fc4663c23a41
fioalpha <fioalpha@gmail.com>
2021-10-19 01:43:12 -0300

    refactor lint

--- app/src/main/java/com/fioalpha/gisthub/MainActivity.kt
+++ app/src/main/java/com/fioalpha/gisthub/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.fioalpha.gisthub
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) }
+        setContentView(R.layout.activity_main)
+    }
 }
