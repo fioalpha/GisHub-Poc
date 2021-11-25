caa08a3d14f75f0790c035944f3b06bd2be13d8c
fioalpha <fioalpha@gmail.com>
2021-11-24 23:16:08 -0300

    feat(di): add module inject dependecies gists feature
    Add module inject dependencies

--- feature/gist/src/main/java/com/fioalpha/gists/di/GistFeatureModule.kt
+++ feature/gist/src/main/java/com/fioalpha/gists/di/GistFeatureModule.kt
@@ -1,1 +1,18 @@
 package com.fioalpha.gists.di
+
+import com.fioalpha.gists.data.GistRepository
+import com.fioalpha.gists.domain.model.Gist
+import com.fioalpha.gists.domain.usecase.FetchListGistUseCase
+import com.fioalpha.gists.domain.usecase.FetchListGistUseCaseImpl
+import org.koin.dsl.module
+
+val gistModule = module {
+    single<GistRepository> {
+        object : GistRepository {
+            override suspend fun getGist(page: Int): List<Gist> {
+                return emptyList()
+            }
+        }
+    }
+    single<FetchListGistUseCase> { FetchListGistUseCaseImpl(gistRepository = get()) }
+}
